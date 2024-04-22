051d70ea06c7879da5ae4d4219905746eef13ed0
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-22 11:55:37 +0300

    Revert "."
    This reverts commit 4bdc59281e42d810a3248d5b888850a630538374.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module com.tugalsan.api.bytes {
     requires com.tugalsan.api.unsafe;
-    requires com.tugalsan.api.union;
     requires com.tugalsan.api.callable;
     exports com.tugalsan.api.bytes.client;
 }
