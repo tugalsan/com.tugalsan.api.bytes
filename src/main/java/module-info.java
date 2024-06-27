c3020568fffb5d095960252cd4636501161c3d6d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:09:57 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module com.tugalsan.api.bytes {
     requires com.tugalsan.api.unsafe;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     exports com.tugalsan.api.bytes.client;
 }
